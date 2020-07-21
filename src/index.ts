2903e06e69416c68a1e679fd963fa5fd5fde08ac
julianahrens <22180888+julianahrens@users.noreply.github.com>
2020-07-21 12:04:37 +0200

    [fix] use JavaScript Standard Style in TypeScript in general files
    - src/index.ts:25:25: Unexpected trailing comma
    - src/util/Cache.ts:32:1: Block must not be padded by blank lines.

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ async function bootstrap (): Promise<void> {
     container: Container,
     emitSchemaFile: {
       commentDescriptions: true,
-      sortedSchema: true,
+      sortedSchema: true
     }
   })
 
